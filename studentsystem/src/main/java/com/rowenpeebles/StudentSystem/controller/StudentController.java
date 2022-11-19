574223773d727d6024b5e1af0011a4c578408001
Rowen Peebles <rowen.peebles@outlook.com>
2022-11-19 21:49:33 +0000

    Adding getAllStudents function to student controller

--- studentsystem/src/main/java/com/rowenpeebles/StudentSystem/controller/StudentController.java
+++ studentsystem/src/main/java/com/rowenpeebles/StudentSystem/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.rowenpeebles.StudentSystem.controller;
 import com.rowenpeebles.StudentSystem.model.Student;
 import com.rowenpeebles.StudentSystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -22,6 +21,11 @@ public class StudentController {
         studentService.saveStudent(student);
         return "New student has been added";
 
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
     }
 
 }
--- studentsystem/src/main/java/com/rowenpeebles/StudentSystem/service/StudentServiceImpl.java
+++ studentsystem/src/main/java/com/rowenpeebles/StudentSystem/service/StudentServiceImpl.java
@@ -5,6 +5,8 @@ import com.rowenpeebles.StudentSystem.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentServiceImpl implements StudentService {
 
@@ -13,6 +15,11 @@ public class StudentServiceImpl implements StudentService {
     @Override
     public Student saveStudent(Student student) {
         return studentRepository.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
     }
 
 }
