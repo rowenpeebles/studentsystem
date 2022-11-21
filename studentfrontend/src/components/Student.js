95c8644fb724e80d0076a181a96c21c6e0fc6c57
Rowen Peebles <rowen.peebles@outlook.com>
2022-11-21 12:01:18 +0000

    Adding registered students form

--- studentfrontend/src/components/Student.js
+++ studentfrontend/src/components/Student.js
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Container } from '@mui/system';
 import Paper from '@mui/material/Paper';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from '@mui/material';
 
 export default function Student() {
@@ -11,6 +11,7 @@ export default function Student() {
     const paperStyle={padding:'50px 20px', width:600, margin:'20px auto'};
     const [name, setName] = useState('');
     const [address, setAddress] = useState('');
+    const [students, setStudents] = useState([]);
     
     const handleClick=(e)=>{
         e.preventDefault()
@@ -26,11 +27,19 @@ export default function Student() {
         })
     }
 
+    useEffect(()=>{
+        fetch("http://localhost:8080/student/getAll")
+        .then(res=>res.json())
+        .then((result)=>{
+          setStudents(result);
+        }
+      )
+      },[])
+
     return (
         <Container>
             <Paper elevation={3} style={paperStyle}>
-                <h1><u>Student System</u></h1>
-                <h3> Add a new student </h3>
+                <h1><u>Add New Student</u></h1>
                 <Box
                     component="form"
                     sx={{
@@ -47,6 +56,17 @@ export default function Student() {
                     onChange={(e)=>setAddress(e.target.value)}/>
                     <Button variant="contained" onClick={handleClick}>Submit</Button>
                 </Box>
+            </Paper>
+
+            <h1><u>Registered Students</u></h1>
+            <Paper elevation={3} style={paperStyle}>
+                {students.map(student=>(
+                    <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left"}} key={student.id}>
+                        Id:{student.id}<br/>
+                        Name:{student.name}<br/>
+                        Address:{student.address}
+                    </Paper>
+                ))}
             </Paper>
         </Container>
     
